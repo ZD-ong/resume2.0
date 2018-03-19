9509c63bae04c9c62006eb7e7c07b92ccd77188d
ZD-ong <384474574@qq.com>
2018-03-19 19:14:44 +0800

    按钮高亮与图片轮播同步

--- scripts/script.js
+++ scripts/script.js
@@ -73,6 +73,7 @@ console.log(TWEEN)
 
 let n
 初始化()
+var whichBtn = 1
 var timer = setTimer()
 $('#window').on('mouseenter', function () {
     window.clearInterval(timer)
@@ -85,10 +86,12 @@ $('#window').on('mouseleave', function () {
 
 function setTimer(){
     return setInterval(() => {
+        whichBtn += 1
         makeLeave(getImage(n))
             .one('transitionend', (e) => {
                 makeEnter($(e.currentTarget))
             })
+        activeButton(getBtn(whichBtn))
         makeCurrent(getImage(n + 1))
         n += 1
     }, 3000)
@@ -96,6 +99,9 @@ function setTimer(){
 
 function getImage(n) {
     return $(`#images > img:nth-child(${x(n)})`)
+}
+function getBtn(n) {
+    return $(`.buttons > span:nth-child(${x(n)})`)
 }
 var length = $('#images > img').length
 function x(n) {
@@ -112,11 +118,14 @@ function 初始化() {
     n = 1
     $(`#images > img:nth-child(${n})`).addClass('current')
         .siblings().addClass('enter')
+    $(`.buttons > span:nth-child(${n})`).addClass('red')
+        .siblings('.red').removeClass('red')
 }
 
 function makeCurrent($node) {
     return $node.removeClass('enter leave').addClass('current')
 }
+
 function makeLeave($node) {
     return $node.removeClass('enter current').addClass('leave')
 }
@@ -124,16 +133,16 @@ function makeEnter($node) {
     return $node.removeClass('leave current').addClass('enter')
 }
 
-var allButtons = $('#buttons > span')
-for(let i = 0; i  < allButtons.length; i++){
-    $(allButtons[i]).on('click',function(e){
-        var index = $(e.currentTarget).index()
-        whichPic = index
-        activeButton(allButtons.eq(whichPic))
-    })
+// var allButtons = $('#buttons > span')
+// for(let i = 0; i  < allButtons.length; i++){
+//     $(allButtons[i]).on('click',function(e){
+//         var index = $(e.currentTarget).index()
+//         whichPic = index
+//         activeButton(allButtons.eq(whichPic))
+//     })
     
-}
-var whichPic = 0
+// }
+// var whichPic = 0
 function activeButton($button){
     $button
         .addClass('red')
